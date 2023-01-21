617b776e64b67ad97917cbe393f965fa79667351
ruiyan-ma <maruiyan17@gmail.com>
2023-01-21 10:15:18 -0800

    update install.sh

--- multipass/install.sh
+++ multipass/install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 sudo apt update -y && sudo apt upgrade -y
+sudo snap refresh
 
 # shell
 sudo apt install -y zsh git vim curl
-sh -c -y "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 sudo apt install -y gcc g++ gdb cgdb valgrind ranger autojump trash-cli tree bat
