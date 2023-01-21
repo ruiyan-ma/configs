5f73adb38ace7c81de8ee0627c81535417850a13
ruiyan-ma <maruiyan17@gmail.com>
2023-01-21 10:20:53 -0800

    add echo y

--- multipass/install.sh
+++ multipass/install.sh
@@ -4,7 +4,7 @@ sudo snap refresh
 
 # shell
 sudo apt install -y zsh git vim curl
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo y | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 sudo apt install -y gcc g++ gdb cgdb valgrind ranger autojump trash-cli tree bat
