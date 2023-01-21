c04f6fef4b654d530a9af2d9f948379da7b58277
ruiyan-ma <maruiyan17@gmail.com>
2023-01-21 01:05:00 -0800

    update ubuntu

---
+++ ubuntu/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+sudo apt update && sudo apt upgrade -y
+sudo snap refresh
+
+# shell
+sudo apt install zsh git curl
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+sudo apt install gcc g++ valgrind ranger autojump trash-cli tree bat
+
+# vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# ranger
+git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
+
+git clone https://github.com/ruiyan-ma/configs.git
+cp configs/ubuntu/zshrc ~/.zshrc
+cp configs/ubuntu/vimrc ~/.vimrc
+cp configs/ubuntu/rc.conf ~/.config/ranger/rc.conf
+rm -rf configs
